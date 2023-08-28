18399f8ef0e5c53974b191601f2b535c7293cb4e
Amal C P <acp070146@gmail.com>
2023-08-28 23:35:12 +0530

    home page desk update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import styles from './Navbar.module.css'
+import React from "react";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className={styles.navbarWrapper}>
+      <a href="">ABOUT</a>
+      <a href="">TIMELINE</a>
+      <a href="">FAQS</a>
+      <a href="">CONTACT</a>
+      <a className={styles.navbarButton} href="">REGISTER</a>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

fd1aa9c11bfa3b02f0b7fc78f67d99ffe1091cf0
Amal C P <acp070146@gmail.com>
2023-08-28 23:13:19 +0530

    home page update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,10 +3,13 @@ import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className={styles.homeWrapper}>
+      <div>
+        <h1 className={styles.hh1}>the hackathon you should <span>never miss</span></h1>
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
 
 export default Home
